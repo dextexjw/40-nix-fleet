ab2f09eb7338e711ab878631a53d26b9d59ee776
dexterjw <dev@jax22.com>
2026-06-02 18:11:27 -0400

    Refactor Paperless service configuration to enhance host name handling and improve URL generation logic

--- modules/productivity/services/paperless.nix
+++ modules/productivity/services/paperless.nix
@@ -13,25 +13,28 @@ let
     inherit config lib pkgs;
   };
   inherit (productivityLib) cfg appdata secretPath serviceHostAliases serviceHosts;
- in
+  paperlessHostNames = [ serviceHosts.paperless ] ++ serviceHostAliases.paperless;
+  paperlessOrigin = hostName: "${if hasSuffix ".h" hostName then "http" else "https"}://${hostName}";
+in
 {
   config = mkIf cfg.enable {
-services.paperless = {
-  enable = true;
-  address = "127.0.0.1";
-  configureNginx = true;
-  consumptionDir = "${appdata}/paperless/consume";
-  dataDir = "${appdata}/paperless";
-  database.createLocally = true;
-  domain = serviceHosts.paperless;
-  mediaDir = "${appdata}/paperless/media";
-  passwordFile = secretPath "paperless-admin-password";
-  settings = {
-    PAPERLESS_ADMIN_USER = "smoke";
-    PAPERLESS_ALLOWED_HOSTS = concatStringsSep "," ([ serviceHosts.paperless ] ++ serviceHostAliases.paperless);
-    PAPERLESS_OCR_LANGUAGE = "eng";
-    PAPERLESS_URL = mkForce "http://${serviceHosts.paperless}";
-  };
-};
+    services.paperless = {
+      enable = true;
+      address = "127.0.0.1";
+      configureNginx = true;
+      consumptionDir = "${appdata}/paperless/consume";
+      dataDir = "${appdata}/paperless";
+      database.createLocally = true;
+      domain = serviceHosts.paperless;
+      mediaDir = "${appdata}/paperless/media";
+      passwordFile = secretPath "paperless-admin-password";
+      settings = {
+        PAPERLESS_ADMIN_USER = "smoke";
+        PAPERLESS_ALLOWED_HOSTS = concatStringsSep "," paperlessHostNames;
+        PAPERLESS_CSRF_TRUSTED_ORIGINS = concatStringsSep "," (map paperlessOrigin paperlessHostNames);
+        PAPERLESS_OCR_LANGUAGE = "eng";
+        PAPERLESS_URL = mkForce (paperlessOrigin serviceHosts.paperless);
+      };
+    };
   };
 }
